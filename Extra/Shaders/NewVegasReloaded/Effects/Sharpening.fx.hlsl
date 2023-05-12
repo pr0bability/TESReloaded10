893c42ebbddcee64a23bd2df98048bd9a70f464a
MathieuC <m.chaptel@gmail.com>
2023-05-12 20:06:21 +0200

    Add new CAS Sharpening shader from Boomstick
    Co-Authored-By: Boomstick467 <81816158+srjtjsykyk@users.noreply.github.com>

--- Extra/Shaders/NewVegasReloaded/Effects/Sharpening.fx.hlsl
+++ Extra/Shaders/NewVegasReloaded/Effects/Sharpening.fx.hlsl
@@ -1,110 +1,101 @@
-//Sharpening fullscreen shader for Oblivion/Skyrim Reloaded
+//Contrast Adaptive Sharpening Enhanced Shader for TESReloaded
+//----------------------------------------------------
+// Features:
+// - Exponential Depth falloff
+// 		Simple depth falloff using a max depth var to calculate a 0-1 fallOffFactor that's then lerped with the final sharpened color
+//		Multipied by 1000 so you have to use lower input params making 0 apply no effect and 1 equal 1000
+// - Intensity
+//		It does what it says, intensity param that's multiplied by the contrast to increase overall sharpening effect
+//		Multipied by two so the effect is more noticable.
+// - Adjustable Kernel size
+//		You just change the kernel size to increase sampling radius of the custom Manhattan sampling thingy
+// - Debug modes
+//		Used for dev mode so you can see each process of the sharpening
+//----------------------------------------------------
 
-#define viewsharpening 0
-#define pattern 6
+#define debugMode 0 // 1 - FalloffFactor 2 - Contrast 3 - Average(blur)
+#define falloffExponent 4 // Set the falloff exponent to control the curve steepness for depth falloff
+#define kernelSize 3 // Set kernel size for averaging, to find total samples multiply the number by it's self
 
+float4 TESR_SharpeningData; // X - Intensity Y - Clamp  Z - Offset(depth falloff) 
 float4 TESR_ReciprocalResolution;
-float4 TESR_SharpeningData;
 
 sampler2D TESR_RenderedBuffer : register(s0) = sampler_state { ADDRESSU = CLAMP; ADDRESSV = CLAMP; MAGFILTER = LINEAR; MINFILTER = LINEAR; MIPFILTER = LINEAR; };
+sampler2D TESR_DepthBuffer : register(s1) = sampler_state { ADDRESSU = CLAMP; ADDRESSV = CLAMP; MAGFILTER = LINEAR; MINFILTER = LINEAR; MIPFILTER = LINEAR; };
 
-static const float3 CoefLuma = {0.2126, 0.7152, 0.0722};
+#include "Includes/Helpers.hlsl"
+#include "Includes/Depth.hlsl"
 
-struct VSOUT {
-	float4 vertPos : POSITION;
-	float2 UVCoord : TEXCOORD0;
+struct VSOUT
+{
+    float4 vertPos : POSITION;
+    float2 UVCoord : TEXCOORD0;
 };
 
-struct VSIN {
-	float4 vertPos : POSITION0;
-	float2 UVCoord : TEXCOORD0;
+struct VSIN
+{
+    float4 vertPos : POSITION0;
+    float2 UVCoord : TEXCOORD0;
 };
 
-VSOUT FrameVS(VSIN IN) {
-	VSOUT OUT = (VSOUT)0.0f;
-	OUT.vertPos = IN.vertPos;
-	OUT.UVCoord = IN.UVCoord;
-	return (OUT);
+VSOUT FrameVS(VSIN IN)
+{
+    VSOUT OUT = (VSOUT) 0.0f;
+    OUT.vertPos = IN.vertPos;
+    OUT.UVCoord = IN.UVCoord;
+    return (OUT);
 }
 
-float4 LumaSharpenPass(VSOUT IN) : COLOR0 {
+float4 ContrastAdaptiveSharpening(VSOUT IN) : COLOR
+{
+    // CAS Params
+    float intensity = TESR_SharpeningData.x * 2; // Multiplied by two so it's more noticeable change
+    float maxDepth = TESR_SharpeningData.z * 1000; // Multiplied by 1000 so the default values start at zero to one
 
-	float3 ori = tex2D(TESR_RenderedBuffer, IN.UVCoord).rgb;
-	float3 sharp_strength_luma = (CoefLuma * TESR_SharpeningData.x);
-	#if pattern == 1	//A (fast) 7 tap gaussian using only 2+1 texture fetches.
-		float3 blur_ori = tex2D(TESR_RenderedBuffer, IN.UVCoord + (float2(TESR_ReciprocalResolution.x, TESR_ReciprocalResolution.y) / 3.0) * TESR_SharpeningData.z).rgb;
-		blur_ori += tex2D(TESR_RenderedBuffer, IN.UVCoord + (float2(-TESR_ReciprocalResolution.x, -TESR_ReciprocalResolution.y) / 3.0) * TESR_SharpeningData.z).rgb;
-		blur_ori /= 2;
-		sharp_strength_luma *= 1.5;
-	#endif
-	#if pattern == 2	//A 9 tap gaussian using 4+1 texture fetches.
-		float3 blur_ori = tex2D(TESR_RenderedBuffer, IN.UVCoord + float2(TESR_ReciprocalResolution.x, -TESR_ReciprocalResolution.y) * 0.5 * TESR_SharpeningData.z).rgb;
-		blur_ori += tex2D(TESR_RenderedBuffer, IN.UVCoord + float2(-TESR_ReciprocalResolution.x, -TESR_ReciprocalResolution.y) * 0.5 * TESR_SharpeningData.z).rgb;
-		blur_ori += tex2D(TESR_RenderedBuffer, IN.UVCoord + float2(TESR_ReciprocalResolution.x, TESR_ReciprocalResolution.y) * 0.5 * TESR_SharpeningData.z).rgb;
-		blur_ori += tex2D(TESR_RenderedBuffer, IN.UVCoord + float2(-TESR_ReciprocalResolution.x, TESR_ReciprocalResolution.y) * 0.5 * TESR_SharpeningData.z).rgb;
-		blur_ori *= 0.25;
-	#endif
-	#if pattern == 3	//An experimental 17 tap gaussian using 4+1 texture fetches.
-		float3 blur_ori = tex2D(TESR_RenderedBuffer, IN.UVCoord + float2(0.4 * TESR_ReciprocalResolution.x, -1.2 * TESR_ReciprocalResolution.y) * TESR_SharpeningData.z).rgb;
-		blur_ori += tex2D(TESR_RenderedBuffer, IN.UVCoord + float2(-1.2 * TESR_ReciprocalResolution.x, -0.4 * TESR_ReciprocalResolution.y) * TESR_SharpeningData.z).rgb;
-		blur_ori += tex2D(TESR_RenderedBuffer, IN.UVCoord + float2(1.2 * TESR_ReciprocalResolution.x, 0.4 * TESR_ReciprocalResolution.y) * TESR_SharpeningData.z).rgb;
-		blur_ori += tex2D(TESR_RenderedBuffer, IN.UVCoord + float2(-0.4 * TESR_ReciprocalResolution.x, 1.2 * TESR_ReciprocalResolution.y) * TESR_SharpeningData.z).rgb;
-		blur_ori *= 0.25;
-		sharp_strength_luma *= 0.51;
-	#endif
-	#if pattern == 4	//A 9 tap high pass (TESR_ReciprocalResolution.yramid filter) using 4+1 texture fetches.
-		float3 blur_ori = tex2D(TESR_RenderedBuffer, IN.UVCoord + float2(0.5 * TESR_ReciprocalResolution.x, -TESR_ReciprocalResolution.y * TESR_SharpeningData.z)).rgb;
-		blur_ori += tex2D(TESR_RenderedBuffer, IN.UVCoord + float2(TESR_SharpeningData.z * -TESR_ReciprocalResolution.x, 0.5 * -TESR_ReciprocalResolution.y)).rgb;
-		blur_ori += tex2D(TESR_RenderedBuffer, IN.UVCoord + float2(TESR_SharpeningData.z * TESR_ReciprocalResolution.x, 0.5 * TESR_ReciprocalResolution.y)).rgb;
-		blur_ori += tex2D(TESR_RenderedBuffer, IN.UVCoord + float2(0.5 * -TESR_ReciprocalResolution.x, TESR_ReciprocalResolution.y * TESR_SharpeningData.z)).rgb;
-		blur_ori /= 4.0;
-		sharp_strength_luma *= 0.666;
-	#endif
-	#if pattern == 5	//A (slower) 9 tap gaussian using 9 texture fetches.
-		half3 blur_ori = tex2D(TESR_RenderedBuffer, IN.UVCoord + float2(-TESR_ReciprocalResolution.x, TESR_ReciprocalResolution.y) * TESR_SharpeningData.z).rgb;
-		blur_ori += tex2D(TESR_RenderedBuffer, IN.UVCoord + float2(TESR_ReciprocalResolution.x, -TESR_ReciprocalResolution.y) * TESR_SharpeningData.z).rgb;
-		blur_ori += tex2D(TESR_RenderedBuffer, IN.UVCoord + float2(-TESR_ReciprocalResolution.x, -TESR_ReciprocalResolution.y)  * TESR_SharpeningData.z).rgb;
-		blur_ori += tex2D(TESR_RenderedBuffer, IN.UVCoord + float2(TESR_ReciprocalResolution.x, TESR_ReciprocalResolution.y) * TESR_SharpeningData.z).rgb;
-		half3 blur_ori2 = tex2D(TESR_RenderedBuffer, IN.UVCoord + float2(0, TESR_ReciprocalResolution.y) * TESR_SharpeningData.z).rgb;
-		blur_ori2 += tex2D(TESR_RenderedBuffer, IN.UVCoord + float2(0, TESR_ReciprocalResolution.y) * TESR_SharpeningData.z).rgb;
-		blur_ori2 += tex2D(TESR_RenderedBuffer, IN.UVCoord + float2(-TESR_ReciprocalResolution.x, 0) * TESR_SharpeningData.z).rgb;
-		blur_ori2 += tex2D(TESR_RenderedBuffer, IN.UVCoord + float2(TESR_ReciprocalResolution.x, 0) * TESR_SharpeningData.z).rgb;
-		blur_ori2 *= 2.0;
-		blur_ori += blur_ori2;
-		blur_ori += (ori * 4);
-		blur_ori /= 16.0;
-	#endif
-	#if pattern == 6	//A (slower) 9 tap high pass using 9 texture fetches.
-		float3 blur_ori = tex2D(TESR_RenderedBuffer, IN.UVCoord + float2(-TESR_ReciprocalResolution.x, TESR_ReciprocalResolution.y) * TESR_SharpeningData.z).rgb;
-		blur_ori += tex2D(TESR_RenderedBuffer, IN.UVCoord + float2(TESR_ReciprocalResolution.x, -TESR_ReciprocalResolution.y) * TESR_SharpeningData.z).rgb;
-		blur_ori += tex2D(TESR_RenderedBuffer, IN.UVCoord + float2(-TESR_ReciprocalResolution.x, -TESR_ReciprocalResolution.y)  * TESR_SharpeningData.z).rgb;
-		blur_ori += tex2D(TESR_RenderedBuffer, IN.UVCoord + float2(TESR_ReciprocalResolution.x, TESR_ReciprocalResolution.y) * TESR_SharpeningData.z).rgb;
-		blur_ori += ori.rgb;
-		blur_ori += tex2D(TESR_RenderedBuffer, IN.UVCoord + float2(0, TESR_ReciprocalResolution.y) * TESR_SharpeningData.z).rgb;
-		blur_ori += tex2D(TESR_RenderedBuffer, IN.UVCoord + float2(0, -TESR_ReciprocalResolution.y) * TESR_SharpeningData.z).rgb;
-		blur_ori += tex2D(TESR_RenderedBuffer, IN.UVCoord + float2(-TESR_ReciprocalResolution.x, 0) * TESR_SharpeningData.z).rgb;
-		blur_ori += tex2D(TESR_RenderedBuffer, IN.UVCoord + float2(TESR_ReciprocalResolution.x, 0) * TESR_SharpeningData.z).rgb;
-		blur_ori /= 9;
-	#endif
+    // Sample the rendered and reconstruct position from depth for depth falloff
+    float4 color = tex2D(TESR_RenderedBuffer, IN.UVCoord);
+    float depth = readDepth(IN.UVCoord);
+
+    // Calculate the falloff factor using pow
+    float falloffFactor = pow(saturate((maxDepth - depth) / maxDepth), falloffExponent);
+
+    // Calculate the average color of the surrounding pixels
+    float3 average = 0;
+    float sum = 0;
+    for (int x = -kernelSize / 2; x <= kernelSize / 2; x++)
+    {
+        for (int y = -kernelSize / 2; y <= kernelSize / 2; y++)
+        {
+            float weight = 1.0 - (abs(x) + abs(y)) / kernelSize; // simulate weights with Manhattan distance
+            average += tex2D(TESR_RenderedBuffer, IN.UVCoord + float2(x, y) * TESR_ReciprocalResolution.xy) * weight;
+            sum += weight;
+        }
+    }
+    average /= sum;
+    
+    // Calculate the contrast between the current pixel and the average color
+    float contrast = dot(abs(color.rgb - average), luma(color.rgb) / (luma(color.rgb) + 0.05));
+    
+    // Calculate the sharpening and lerp with fallofffactor
+    color.rgb = lerp(color.rgb, color.rgb + (color.rgb - average) * saturate(contrast * intensity), falloffFactor);
+    
+    // Debug modes
+    #if debugMode == 1
+        return falloffFactor;
+    #elif debugMode == 2
+        return contrast * intensity;
+    #elif debugMode == 3
+        return float4(average, 1.0);   
+    #endif
 	
-	float3 sharp = ori - blur_ori;
-	float4 sharp_strength_luma_clamp = float4(sharp_strength_luma * (0.5 / TESR_SharpeningData.y), 0.5);
-	float sharp_luma = saturate(dot(float4(sharp, 1.0), sharp_strength_luma_clamp));
-	sharp_luma = (TESR_SharpeningData.y * 2.0) * sharp_luma - TESR_SharpeningData.y;
-	ori.rgb = ori.rgb + sharp_luma;
-	
-	#if viewsharpening
-		return float4(saturate(0.5 + (sharp_luma * 4)).rrr, 1);
-	#endif
-	
-	return float4(saturate(ori.rgb), 1);
+    return color;
 }
-	
+
 technique
 {
-	pass
-	{
-		VertexShader = compile vs_3_0 FrameVS();
-		PixelShader  = compile ps_3_0 LumaSharpenPass();
-	}
+    pass
+    {
+        VertexShader = compile vs_3_0 FrameVS();
+        PixelShader = compile ps_3_0 ContrastAdaptiveSharpening();
+    }
 }
