8f9291ac68cf60010115f998b3f849cde49e390b
MathieuC <m.chaptel@gmail.com>
2023-08-26 22:01:21 +0200

    optimize SKIN2000 with Skin.hlsl include and added customization

--- Extra/Shaders/NewVegasReloaded/Shaders/Includes/Skin.hlsl
+++ Extra/Shaders/NewVegasReloaded/Shaders/Includes/Skin.hlsl
@@ -37,3 +37,49 @@ float3 Skin(float3 SkinColor, float3 LightColor, float3 CameraDir, float3 LightD
 	return finalCol.rgb;
 	
 };
+
+
+float3 GetLighting(float3 lightDir, float3 eyeDir, float3 normal, float3 lightColor){
+    float fresnelCoeff = pows(1 - shades(normal, eyeDir), 5);
+    float diffuse = shades(normal, lightDir);
+    float3 fresnel = fresnelCoeff * lightColor * 0.5 * pow(diffuse, TESR_DebugVar.w);
+    float3 lighting = diffuse * lightColor + fresnel + AmbientColor.rgb;
+    return max(lighting, 0);
+}
+
+float GetSpecular(float3 lightDirection, float3 eyeDirection, float3 normal, float3 lightColor){
+    return  pow(shades(normal, normalize(lightDirection + eyeDirection)), TESR_DebugVar.z) * luma(lightColor) * TESR_DebugVar.y;
+}
+
+float GetSSS(float3 lightDirection, float3 normal){
+    return (1 - shades(normal, lightDirection)) * float3(0.5, 0.2, 0.3) * TESR_DebugVar.x;
+}
+
+float3 getNormal(float2 uv){
+    return (expand(tex2D(NormalMap, uv).xyz));
+}
+
+// float2 getScreenpos(VS_INPUT IN){
+//     return IN.position.xy * TESR_ReciprocalResolution.xy;
+// };
+
+
+float3 ApplyVertexColor(float3 baseColor, float3 vertexColor, float4 toggles){
+   return toggles.x <= 0.0 ? baseColor : (baseColor.rgb * vertexColor); // apply vertex color
+}
+
+float3 ApplyFog(float3 baseColor, float4 fogColor, float4 toggles){
+    return Toggles.y <= 0.0 ? baseColor : ((fogColor.a * (fogColor.rgb - baseColor)) + baseColor);
+}
+
+float4 getBaseColor(float2 uv, sampler2D FaceGenMap0Buffer, sampler2D FaceGenMap1Buffer, sampler2D BaseColorBuffer){
+    // sample color textures
+    float3 faceGenMap0 = tex2D(FaceGenMap0Buffer, uv).rgb;
+    float3 faceGenMap1 = tex2D(FaceGenMap1Buffer, uv).rgb;
+    float4 baseTexture = tex2D(BaseColorBuffer, uv);
+    float4 baseColor = float4(0, 0, 0, 1);
+	baseColor.rgb = 2 * ((expand(faceGenMap0) + baseTexture.rgb) * (2 * faceGenMap1));
+    baseColor.a = baseTexture.a;
+    
+    return baseColor;
+}
--- Extra/Shaders/NewVegasReloaded/Shaders/SKIN2000.pso.hlsl
+++ Extra/Shaders/NewVegasReloaded/Shaders/SKIN2000.pso.hlsl
@@ -8,7 +8,12 @@ sampler2D FaceGenMap0 : register(s2);
 sampler2D FaceGenMap1 : register(s3);
 sampler2D NormalMap : register(s1);
 float4 PSLightColor[10] : register(c3);
-float4 Toggles : register(c27);
+float4 Toggles : register(c27); // x:bUseVertexColors, y:fUnknown_FogRelated, z:fSpecularity, w:fAlphaTestRef
+
+float4 TESR_ReciprocalResolution;
+float4 TESR_SkinData;
+float4 TESR_SkinColor;
+float4 TESR_DebugVar;
 
 
 #include "Includes/Helpers.hlsl"
@@ -33,6 +38,7 @@ struct VS_INPUT {
     float2 BaseUV : TEXCOORD0;			            // UV
     float3 texcoord_1 : TEXCOORD1;			// light data in tangent space
     float3 texcoord_6 : TEXCOORD6;			// eye data in tangent space
+    float3 position : SV_POSITION;			// light data in tangent space
     float3 color_0 : COLOR0;                // vertex color?
     float4 color_1 : COLOR1;                // fog contribution?
 };
@@ -41,6 +47,8 @@ struct VS_OUTPUT {
     float4 color_0 : COLOR0;
 };
 
+#include "Includes/Skin.hlsl"
+
 
 VS_OUTPUT main(VS_INPUT IN) {
     VS_OUTPUT OUT;
@@ -48,30 +56,23 @@ VS_OUTPUT main(VS_INPUT IN) {
     // base geometry information
     float3 lightDirection = normalize(IN.texcoord_1);
     float3 eyeDirection = normalize(IN.texcoord_6);
-    float3 normal = normalize(expand(tex2D(NormalMap, IN.BaseUV.xy).xyz));
+    float3 normal = getNormal(IN.BaseUV);
 
     // calculate lighting components
-    float fresnelCoeff = 1 - shades(normal, eyeDirection);
-    float3 fresnel = (shades(lightDirection, -eyeDirection) * sqr(fresnelCoeff) * PSLightColor[0].rgb) * 0.5;
-    float3 lighting = ((shades(normal, lightDirection) * PSLightColor[0].rgb) + fresnel) + AmbientColor.rgb;
+    float3 lighting = GetLighting(lightDirection, eyeDirection, normal, PSLightColor[0].rgb);
+    float3 sss = GetSSS(lightDirection, normal) * AmbientColor.rgb;
+    float spec = GetSpecular(lightDirection, eyeDirection, normal, PSLightColor[0].rgb);
 
-    // sample color textures
-    float3 faceGenMap0 = tex2D(FaceGenMap0, IN.BaseUV.xy).rgb;
-    float3 faceGenMap1 = tex2D(FaceGenMap1, IN.BaseUV.xy).rgb;
-    float4 baseTexture = tex2D(BaseMap, IN.BaseUV.xy);
-    float3 baseColor = 2 * ((expand(faceGenMap0) + baseTexture.rgb) * (2 * faceGenMap1));
+    float4 baseColor = getBaseColor(IN.BaseUV, FaceGenMap0, FaceGenMap1, BaseMap);
+    baseColor.rgb = ApplyVertexColor(baseColor.rgb, IN.color_0.rgb, Toggles);
 
-    baseColor = (Toggles.x <= 0.0 ? baseColor : (baseColor * IN.color_0.rgb)); // apply vertex color
-    float4 color = (AmbientColor.a >= 1 ? 0 : (baseTexture.a - Toggles.w));
+    float4 color = AmbientColor.a >= 1 ? 0 : (baseColor.a - Toggles.w);
+    float3 finalColor = lighting * baseColor.rgb + sss + spec;
 
-    float3 finalColor = max(lighting, 0) * baseColor;
+    color.rgb = ApplyFog(finalColor, IN.color_1, Toggles);
+    color.a = baseColor.a * AmbientColor.a;
 
-    color.rgb = Toggles.y <= 0.0 ? finalColor : ((IN.color_1.a * (IN.color_1.rgb - finalColor)) + finalColor); // apply fog color contribution
-    color.a = baseTexture.a * AmbientColor.a;
-
-    // OUT.color_0 = color;
-    OUT.color_0 = float4(IN.BaseUV, 0, 1);
-
+    OUT.color_0 = color;
     return OUT;
 };
 
