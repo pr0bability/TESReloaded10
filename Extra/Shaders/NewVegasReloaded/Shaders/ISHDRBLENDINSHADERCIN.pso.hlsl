711d4d335738ec06e9911f3bd6495dcce7124aa0
MathieuC <m.chaptel@gmail.com>
2023-10-22 01:16:16 +0200

    add new tonemappers and settings to cinematic HDR shaders

--- Extra/Shaders/NewVegasReloaded/Shaders/ISHDRBLENDINSHADERCIN.pso.hlsl
+++ Extra/Shaders/NewVegasReloaded/Shaders/ISHDRBLENDINSHADERCIN.pso.hlsl
@@ -5,13 +5,16 @@
 
 sampler2D ScreenSpace : register(s0);  // bloom buffer
 sampler2D DestBlend : register(s1);    // base non tonemapped image
+
 float4 HDRParam : register(c1);
 float3 BlurScale : register(c2);
-float4 Cinematic : register(c19); // x:saturation, y:brightness, z:contrast
+float4 Cinematic : register(c19); // x:saturation, y:avgluma, z:brightness, w: contrast
 float4 Tint : register(c20);  // weather Cinematic IS tint?
 float4 Fade : register(c22);  // Night eye color
 float4 TESR_DebugVar : register(c24);
-
+float4 TESR_HDRBloomData : register(c25);
+float4 TESR_SunAmount : register(c26);
+float4 TESR_HDRData : register(c27);
 
 // Registers:
 //
@@ -43,8 +46,7 @@ struct VS_OUTPUT {
 
 
 // ACES tonemapping https://knarkowicz.wordpress.com/2016/01/06/aces-filmic-tone-mapping-curve/
-float3 ACESFilm(float3 x)
-{
+float3 ACESFilm(float3 x) {
 	float a = 2.51f;
 	float b = 0.03f;
 	float c = 2.43f;
@@ -53,36 +55,47 @@ float3 ACESFilm(float3 x)
 	return saturate((x*(a*x+b))/(x*(c*x+d)+e));
 }
 
+float3 Reinhard(float3 x, float whitepoint) {
+    return x * (1 + x / whitepoint.xxx)/(1 + x);
+}
+
+float3 Uncharted2Tonemap(float3 x) {
+    float A = 0.15;
+    float B = 0.50;
+    float C = 0.10;
+    float D = 0.20;
+    float E = 0.02;
+    float F = 0.30;
+    float W = 11.2;
+    return ((x*(A*x+C*B)+D*E)/(x*(A*x+B)+D*F))-E/F;
+}
+
 VS_OUTPUT main(VS_INPUT IN) {
     VS_OUTPUT OUT;
 
-    // blends bloom
-
-    // const float4 const_3 = {0.299, 0.587000012, 0.114, 0};
-
     float4 bloom = tex2D(ScreenSpace, IN.ScreenOffset.xy);
-    float4 hdrImage = tex2D(DestBlend, IN.texcoord_1.xy);
-    
+    bloom.rgb = TESR_HDRBloomData.x * pow(max(bloom.rgb, 0), TESR_HDRBloomData.y);
+    float4 hdrImage = tex2D(DestBlend, IN.texcoord_1.xy) * TESR_HDRData.y; // exposure
+
     float4 final = hdrImage;
     final.w = BlurScale.z;
 
-    float HDR = HDRParam.x * TESR_DebugVar.z; // brights cutoff?
+    float HDR = HDRParam.x; // brights cutoff?
 
-    float q0 = 1.0 / max(bloom.w, HDR) * TESR_DebugVar.y;
-    float3 q1 = ((q0 * HDR) * final.rgb) + max(bloom.xyz * (q0 * 0.5), 0);
+    float q0 = 1.0 / max(bloom.w, HDR);
+    final.rgb = ((q0 * HDR) * final.rgb) + max(bloom.rgb * (q0 * 0.5), 0); // blend image and bloom
 
-    float screenluma = luma(q1);
-    
-    float3 q3 = lerp(screenluma.xxx, q1.rgb, Cinematic.x * TESR_DebugVar.x); // saturation
-    float3 q2 = q3 + (Tint.a * ((screenluma * Tint.rgb) - q3)); // apply tint
-    float3 q4 = Cinematic.w * q2 - Cinematic.y;
-    final.xyz = lerp((Cinematic.z * q4) + Cinematic.y, Fade.xyz, Fade.w); // apply night fade
+    float screenluma = luma(final.rgb);
+    final.rgb = lerp(screenluma.xxx, final.rgb, Cinematic.x * TESR_HDRData.z); // saturation
+    float3 q2 = lerp(final.rgb, Tint.rgb * screenluma, Tint.a); // apply tint
+
+    float3 oldTonemap = Cinematic.z * (Cinematic.w * q2.rgb - Cinematic.y) + Cinematic.y; // apply cinematic brightness & contrast modifiers
+    final.rgb = selectColor(TESR_HDRData.x * 0.1, oldTonemap, ACESFilm(q2), Reinhard(q2, TESR_HDRBloomData.w), Uncharted2Tonemap(q2), q2, q2, q2, q2, q2, q2 ); // tonemap
+
+    final.rgb = lerp(final.rgb, Fade.rgb, Fade.a * (1 - luma(q2))); // apply night eye only to darker parts of the scene to avoid dulling bloom
 
     OUT.color_0.a = 1;
-    OUT.color_0.rgb = final;
-    // OUT.color_0.rgb = ACESFilm(hdrImage + bloom);
-    OUT.color_0.rgb = selectColor(TESR_DebugVar.w, final, (Cinematic.z * q4) + Cinematic.y, hdrImage, q1, q3, q4, bloom, q0, Tint, Fade);
-    // OUT.color_0.rgb = Tint;
+    OUT.color_0.rgb = pow(final.rgb, TESR_HDRData.w);
 
     return OUT;
 };
