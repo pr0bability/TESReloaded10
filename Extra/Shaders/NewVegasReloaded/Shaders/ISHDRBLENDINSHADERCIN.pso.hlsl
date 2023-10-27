756c116e8658de1d922bbf362c29615c10033b41
MathieuC <m.chaptel@gmail.com>
2023-10-27 22:05:04 +0200

    scale bloom in a way that conserves color

--- Extra/Shaders/NewVegasReloaded/Shaders/ISHDRBLENDINSHADERCIN.pso.hlsl
+++ Extra/Shaders/NewVegasReloaded/Shaders/ISHDRBLENDINSHADERCIN.pso.hlsl
@@ -53,10 +53,13 @@ struct VS_OUTPUT {
 VS_OUTPUT main(VS_INPUT IN) {
     VS_OUTPUT OUT;
 
+    // scale bloom while maintaining color
     float4 bloom = tex2D(ScreenSpace, IN.ScreenOffset.xy);
-    bloom.rgb = TESR_HDRBloomData.x * pows(max(bloom.rgb, 0), TESR_HDRBloomData.y);
-    float4 hdrImage = tex2D(DestBlend, IN.texcoord_1.xy); 
+    float maxBloom = max(bloom.r, max(bloom.g, bloom.b));
+    float4 ratio = bloom / maxBloom;
+    bloom.rgb = TESR_HDRBloomData.x * pows(max(maxBloom, 0), TESR_HDRBloomData.y) * ratio;
 
+    float4 hdrImage = tex2D(DestBlend, IN.texcoord_1.xy); 
     float3 final = pows(hdrImage.rgb, 1/TESR_ToneMapping.w) * TESR_HDRData.y; // linearize & exposure
 
     float HDR = HDRParam.x; // brights cutoff?
