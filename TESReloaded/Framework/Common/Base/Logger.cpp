aef94d430c43cdd03e4d2fddffee0d40420bcf28
MathieuC <m.chaptel@gmail.com>
2023-11-06 00:29:44 +0700

    add Logger::TraceRenderState function to output current renderstate settings

--- TESReloaded/Framework/Common/Base/Logger.cpp
+++ TESReloaded/Framework/Common/Base/Logger.cpp
@@ -95,3 +95,32 @@ void Logger::Debug(const char* Message, ...) {
 	}
 #endif // debugmode
 }
+
+void Logger::TraceRenderState() {
+	NiDX9RenderState* RenderState = TheRenderManager->renderState;
+
+	Logger::Log("==== Render State ====");
+
+	Logger::Log("Render State D3DRS_ZENABLE %i", RenderState->GetRenderState(D3DRS_ZENABLE));
+	Logger::Log("Render State D3DRS_FILLMODE %i", RenderState->GetRenderState(D3DRS_FILLMODE));
+	Logger::Log("Render State D3DRS_SHADEMODE %i", RenderState->GetRenderState(D3DRS_SHADEMODE));
+	Logger::Log("Render State D3DRS_ZWRITEENABLE %i", RenderState->GetRenderState(D3DRS_ZWRITEENABLE));
+	Logger::Log("Render State D3DRS_ALPHATESTENABLE %i", RenderState->GetRenderState(D3DRS_ALPHATESTENABLE));
+	Logger::Log("Render State D3DRS_LASTPIXEL %i", RenderState->GetRenderState(D3DRS_LASTPIXEL));
+	Logger::Log("Render State D3DRS_SRCBLEND %i", RenderState->GetRenderState(D3DRS_SRCBLEND));
+	Logger::Log("Render State D3DRS_DESTBLEND %i", RenderState->GetRenderState(D3DRS_DESTBLEND));
+	Logger::Log("Render State D3DRS_CULLMODE %i", RenderState->GetRenderState(D3DRS_CULLMODE));
+	Logger::Log("Render State D3DRS_ZFUNC %i", RenderState->GetRenderState(D3DRS_ZFUNC));
+	Logger::Log("Render State D3DRS_ALPHAREF %i", RenderState->GetRenderState(D3DRS_ALPHAREF));
+	Logger::Log("Render State D3DRS_ALPHAFUNC %i", RenderState->GetRenderState(D3DRS_ALPHAFUNC));
+	Logger::Log("Render State D3DRS_DITHERENABLE %i", RenderState->GetRenderState(D3DRS_DITHERENABLE));
+	Logger::Log("Render State D3DRS_ALPHABLENDENABLE %i", RenderState->GetRenderState(D3DRS_ALPHABLENDENABLE));
+	Logger::Log("Render State D3DRS_FOGENABLE %i", RenderState->GetRenderState(D3DRS_FOGENABLE));
+	Logger::Log("Render State D3DRS_SPECULARENABLE %i", RenderState->GetRenderState(D3DRS_SPECULARENABLE));
+	Logger::Log("Render State D3DRS_FOGCOLOR %i", RenderState->GetRenderState(D3DRS_FOGCOLOR));
+	Logger::Log("Render State D3DRS_FOGTABLEMODE %i", RenderState->GetRenderState(D3DRS_FOGTABLEMODE));
+	Logger::Log("Render State D3DRS_FOGSTART %i", RenderState->GetRenderState(D3DRS_FOGSTART));
+	Logger::Log("Render State D3DRS_FOGEND %i", RenderState->GetRenderState(D3DRS_FOGEND));
+	Logger::Log("Render State D3DRS_POINTSIZE %i", RenderState->GetRenderState(D3DRS_POINTSIZE));
+	Logger::Log("Render State D3DRS_COLORWRITEENABLE %i", RenderState->GetRenderState(D3DRS_COLORWRITEENABLE));
+}
