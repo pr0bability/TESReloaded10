f91679ba1e25c0f97b75d72f7c33c81bae66d6fe
MathieuC <m.chaptel@gmail.com>
2023-11-11 14:05:03 +0700

    fix dayTime/dayLight/ShadowFade constant calculation during sunset/sunrise transitions

--- Extra/Shaders/NewVegasReloaded/Effects/ShadowsExteriors.fx.hlsl
+++ Extra/Shaders/NewVegasReloaded/Effects/ShadowsExteriors.fx.hlsl
@@ -4,7 +4,7 @@
 float4 TESR_ReciprocalResolution;
 float4 TESR_WaterSettings; //x: water height in the cell, y: water depth darkness, z: is camera underwater
 float4 TESR_ShadowData; // x: quality, y: darkness, z: nearmap resolution, w: farmap resolution
-float4 TESR_ShadowFade;
+float4 TESR_ShadowFade; // x: fading at sunrise/sunset, y:disabled shadows, z: pointlights shadows
 float4 TESR_ShadowRadius; // radius of the 4 cascades
 float4 TESR_SkyColor;
 float4 TESR_SunAmbient;
@@ -58,51 +58,32 @@ float4 Shadow(VSOUT IN) : COLOR0
 	float depth = readDepth(uv);
 	float3 camera_vector = toWorld(uv) * depth;
 	float uniformDepth = length(camera_vector);
-
 	float4 world_pos = float4(TESR_CameraPosition.xyz + camera_vector, 1.0f);
 	float3 world_normal = GetWorldNormal(IN.UVCoord);
 
 	// early out for underwater surface (if camera is underwater and surface to shade is close to water level with normal pointing downward)
 	if (TESR_WaterSettings.z == 1 && world_pos.z < (TESR_WaterSettings.x + 2) && world_pos.z > (TESR_WaterSettings.x - 2) && dot(world_normal, float3(0, 0, -1)) > 0.999) return color;
 
-	float4 Shadow = tex2D(TESR_PointShadowBuffer, IN.UVCoord);
+	float2 Shadow = tex2D(TESR_PointShadowBuffer, IN.UVCoord).rg;
 
-	Shadow.r = lerp(Shadow.r, 1.0f, TESR_ShadowFade.x); // apply darkness fading when sun is low or moon is not full
-
-	float ambient = lerp(1, luma(TESR_SunAmbient), TESR_ShadowFade.z * TESR_ShadowFade.x); //ShadowFade.z means point Lights are on
-	Shadow.r = lerp(0, ambient, Shadow.r); // scale shadows strength to ambient before adding attenuation
+	// scale shadows strength to ambient before adding attenuation for pointlights (ShadowFade.z means point Lights are on)
+	float ambient = lerp(1, luma(TESR_SunAmbient), TESR_ShadowFade.z * TESR_ShadowFade.x);
+	Shadow.r = lerp(0, ambient, Shadow.r);
 	Shadow.r += Shadow.g; // Apply poing light attenuation
 
 	float darkness = lerp(DARKNESS, 1.0f, TESR_ShadowFade.x);	// fade shadows to light when sun is low
-	Shadow = lerp(darkness, 1.0f, Shadow); 	// brighten shadow value from 0 to darkness from config value
+	Shadow.r = lerp(darkness, 1.0, Shadow.r); 	// brighten shadow value from 0 to darkness from config value
 
 	// No point for the shadow buffer to be beyond the 0-1 range
-	Shadow = saturate(Shadow);
+	Shadow.r = saturate(Shadow.r);
 
 #if viewshadows == 1
 	return Shadow;
 #endif
 
-	// quick average lum with 4 samples at corner pixels
-	float4 emissives = tex2D(TESR_RenderedBuffer, uv + float2(-0.5, -0.5) * TESR_ReciprocalResolution.xy);
-	emissives += tex2D(TESR_RenderedBuffer, uv + float2(-0.5, 0.5) * TESR_ReciprocalResolution.xy);
-	emissives += tex2D(TESR_RenderedBuffer, uv + float2(0.5, -0.5) * TESR_ReciprocalResolution.xy);
-	emissives += tex2D(TESR_RenderedBuffer, uv + float2(0.5, 0.5) * TESR_ReciprocalResolution.xy);
-	emissives /= 4;
-
-	float threshold = 0.9 * max(luma(TESR_SunAmbient), luma(TESR_SunColor)); // scaling the luma treshold with sun intensity // 2
-	float brightness = luma(color);
-	float bloomScale = 0.1; 
-
-	float bloom = bloomScale * sqr(max(0.0, brightness - threshold)) / brightness;
-	bloom *= brightness * 100;
-	bloom = 1 - saturate(bloom);
-
 	// tint shadowed areas with Sky color before blending
-	// float4 colorShadow = luma(color.rgb) * (Shadow.r * bloom) * TESR_SkyColor;
 	float4 colorShadow = luma(color.rgb) * (Shadow.r) * TESR_SkyColor;
 	return float4(lerp(colorShadow, color * Shadow.r, saturate(Shadow.r + 0.2)).rgb, 1); // bias the transition between the 2 colors to make it less noticeable
-	// return color * Shadow.r; // bias the transition between the 2 colors to make it less noticeable
 }
 
 
