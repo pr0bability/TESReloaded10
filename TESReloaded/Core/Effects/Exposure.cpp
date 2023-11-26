f94be7c68ede68309f1d4637fd6622b817d356ed
MathieuC <m.chaptel@gmail.com>
2023-11-26 21:56:09 +0700

    migrate Exposure

---
+++ TESReloaded/Core/Effects/Exposure.cpp
@@ -0,0 +1,16 @@
+#include "Exposure.h"
+
+void ExposureEffect::UpdateConstants() {
+	TheShaderManager->avglumaRequired = true; // mark average luma calculation as necessary
+
+	bool isExterior = TheShaderManager->isExterior;
+	float transitionCurve = TheShaderManager->transitionCurve;
+
+	if (TheSettingManager->SettingsChanged || TheShaderManager->isDayTimeChanged) {
+		Constants.Data.x = TheSettingManager->GetSettingTransition("Shaders.Exposure", "MinBrightness", isExterior, transitionCurve);
+		Constants.Data.y = TheSettingManager->GetSettingTransition("Shaders.Exposure", "MaxBrightness", isExterior, transitionCurve);
+		Constants.Data.z = TheSettingManager->GetSettingTransition("Shaders.Exposure", "DarkAdaptSpeed", isExterior, transitionCurve);
+		Constants.Data.w = TheSettingManager->GetSettingTransition("Shaders.Exposure", "LightAdaptSpeed", isExterior, transitionCurve);
+	}
+}
+
