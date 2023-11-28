2e97272e78ebd019acd0eae939e8ad57e05048a7
MathieuC <m.chaptel@gmail.com>
2023-11-28 22:15:44 +0700

    fix some shadows issues (?)

--- TESReloaded/Core/Effects/ShadowsExterior.cpp
+++ TESReloaded/Core/Effects/ShadowsExterior.cpp
@@ -4,5 +4,5 @@ void ShadowsExteriorEffect::UpdateConstants() {
 	Constants.ScreenSpaceData.x = TheSettingManager->GetSettingI("Shaders.ShadowsExteriors.ScreenSpace", "Enabled") && Enabled;
 	Constants.ScreenSpaceData.y = TheSettingManager->GetSettingF("Shaders.ShadowsExteriors.ScreenSpace", "BlurRadius");
 	Constants.ScreenSpaceData.z = TheSettingManager->GetSettingF("Shaders.ShadowsExteriors.ScreenSpace", "RenderDistance");
-	Constants.ScreenSpaceData.z = 0;
+	//Constants.ScreenSpaceData.z = 0;
 }
