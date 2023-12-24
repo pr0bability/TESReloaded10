d2f9982f1f6ae33f40928c6553c03ef76b406534
MathieuC <m.chaptel@gmail.com>
2023-12-24 22:10:16 +0700

    Fix sun appearing at night

--- Extra/Shaders/NewVegasReloaded/Shaders/Includes/Sky.hlsl
+++ Extra/Shaders/NewVegasReloaded/Shaders/Includes/Sky.hlsl
@@ -14,9 +14,11 @@ float3 GetSkyColor(float verticality, float athmosphere, float sunHeight, float 
     skyLowColor = pows(skyLowColor, 2.2);
     horizonColor = pows(horizonColor, 2.2);
 
+    float isDayTime = smoothstep(0, 0.5, TESR_SunAmount.x);
+
     float3 color = lerp(skyLowColor.rgb, skyColor.rgb, verticality); // fade from low sky to high sky with height
     color = lerp(color, horizonColor.rgb, saturate(athmosphere * (0.5 + sunInfluence * 0.5))); // fade from base color to horizon color in athmosphere (stronger on the sun side)
-    color += sunColor * sunInfluence * (1.0 - sunHeight) * (((1.0 - verticality) + athmosphere)/2) * sunStrength;
+    color += lerp(0.0, sunColor * sunInfluence * (1.0 - sunHeight) * (((1.0 - verticality) + athmosphere)/2) * sunStrength, isDayTime);
 
     return color;
 }
