04f47050e7e871c6168b63324e4871896a76df67
MathieuC <m.chaptel@gmail.com>
2024-01-06 23:36:43 +0100

    adjustments in update settings functions

--- TESReloaded/Core/Effects/Coloring.cpp
+++ TESReloaded/Core/Effects/Coloring.cpp
@@ -1,33 +1,29 @@
 #include "Coloring.h"
 
 void ColoringEffect::UpdateConstants() {
-	if (Enabled) {
-		SettingsColoringStruct* scs = TheSettingManager->GetSettingsColoring(Player->parentCell->GetEditorName());
-		if (!scs && TheShaderManager->isExterior) scs = TheSettingManager->GetSettingsColoring(Player->GetWorldSpace()->GetEditorName());
-		if (!scs) scs = TheSettingManager->GetSettingsColoring("Default");
+	SettingsColoringStruct* scs = TheSettingManager->GetSettingsColoring(Player->parentCell->GetEditorName());
+	if (!scs && TheShaderManager->isExterior) scs = TheSettingManager->GetSettingsColoring(Player->GetWorldSpace()->GetEditorName());
+	if (!scs) scs = TheSettingManager->GetSettingsColoring("Default");
 
-		Constants.Data.x = scs->Strength;
-		Constants.Data.y = scs->BaseGamma;
-		Constants.Data.z = scs->Fade;
-		Constants.Data.w = scs->Contrast;
-		Constants.Values.x = scs->Saturation;
-		Constants.Values.y = scs->Bleach;
-		Constants.Values.z = scs->BleachLuma;
-		Constants.Values.w = scs->Linearization;
-		Constants.ColorCurve.x = scs->ColorCurve;
-		Constants.ColorCurve.y = scs->ColorCurveR;
-		Constants.ColorCurve.z = scs->ColorCurveG;
-		Constants.ColorCurve.w = scs->ColorCurveB;
-		Constants.EffectGamma.x = scs->EffectGamma;
-		Constants.EffectGamma.y = scs->EffectGammaR;
-		Constants.EffectGamma.z = scs->EffectGammaG;
-		Constants.EffectGamma.w = scs->EffectGammaB;
-	}
+	Constants.Data.x = scs->Strength;
+	Constants.Data.y = scs->BaseGamma;
+	Constants.Data.z = scs->Fade;
+	Constants.Data.w = scs->Contrast;
+	Constants.Values.x = scs->Saturation;
+	Constants.Values.y = scs->Bleach;
+	Constants.Values.z = scs->BleachLuma;
+	Constants.Values.w = scs->Linearization;
+	Constants.ColorCurve.x = scs->ColorCurve;
+	Constants.ColorCurve.y = scs->ColorCurveR;
+	Constants.ColorCurve.z = scs->ColorCurveG;
+	Constants.ColorCurve.w = scs->ColorCurveB;
+	Constants.EffectGamma.x = scs->EffectGamma;
+	Constants.EffectGamma.y = scs->EffectGammaR;
+	Constants.EffectGamma.z = scs->EffectGammaG;
+	Constants.EffectGamma.w = scs->EffectGammaB;
 }
 
-void ColoringEffect::UpdateSettings() {
-
-}
+void ColoringEffect::UpdateSettings() {}
 
 void ColoringEffect::RegisterConstants() {
 	TheShaderManager->ConstantsTable["TESR_ColoringColorCurve"] = &Constants.ColorCurve;
