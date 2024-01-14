509ff1ff0272e8f926e4c95077cea6ee2f83c099
MathieuC <m.chaptel@gmail.com>
2024-01-14 23:31:02 +0100

    fix for ambient occlusion settings retrieval issue

--- TESReloaded/Core/Effects/AmbientOcclusion.cpp
+++ TESReloaded/Core/Effects/AmbientOcclusion.cpp
@@ -12,15 +12,12 @@ void AmbientOcclusionEffect::UpdateSettings() {
 	const char* sectionName = TheShaderManager->GameState.isExterior?"Shaders.AmbientOcclusion.Exteriors":"Shaders.AmbientOcclusion.Interiors";
 
 	Constants.Enabled = TheSettingManager->GetSettingI(sectionName, "Enabled");
-	if (Constants.Enabled) {
-		Constants.AOData.x = TheSettingManager->GetSettingF(sectionName, "Samples");
-		Constants.AOData.y = TheSettingManager->GetSettingF(sectionName, "StrengthMultiplier");
-		Constants.AOData.z = TheSettingManager->GetSettingF(sectionName, "ClampStrength");
-		Constants.AOData.w = TheSettingManager->GetSettingF(sectionName, "Range");
-		Constants.Data.x = TheSettingManager->GetSettingF(sectionName, "AngleBias");
-		Constants.Data.y = TheSettingManager->GetSettingF(sectionName, "LumThreshold");
-		Constants.Data.z = TheSettingManager->GetSettingF(sectionName, "BlurDropThreshold");
-		Constants.Data.w = TheSettingManager->GetSettingF(sectionName, "BlurRadiusMultiplier");
-	}
-
+	Constants.AOData.x = TheSettingManager->GetSettingF(sectionName, "Samples");
+	Constants.AOData.y = TheSettingManager->GetSettingF(sectionName, "StrengthMultiplier");
+	Constants.AOData.z = TheSettingManager->GetSettingF(sectionName, "ClampStrength");
+	Constants.AOData.w = TheSettingManager->GetSettingF(sectionName, "Range");
+	Constants.Data.x = TheSettingManager->GetSettingF(sectionName, "AngleBias");
+	Constants.Data.y = TheSettingManager->GetSettingF(sectionName, "LumThreshold");
+	Constants.Data.z = TheSettingManager->GetSettingF(sectionName, "BlurDropThreshold");
+	Constants.Data.w = TheSettingManager->GetSettingF(sectionName, "BlurRadiusMultiplier");
 }
