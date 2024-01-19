468671051655470196f3ba4810f9f5166e8d3c8c
MathieuC <m.chaptel@gmail.com>
2024-01-19 19:47:08 +0100

    inline util functions

--- TESReloaded/Framework/Common/Base/Utils.h
+++ TESReloaded/Framework/Common/Base/Utils.h
@@ -1,22 +1,22 @@
 #pragma once
 
-static std::string ltrim(const std::string s)
+static inline std::string ltrim(const std::string s)
 {
 	return std::regex_replace(s, std::regex("^\\s+"), "");
 }
 
 
-static std::string rtrim(const std::string s)
+static inline std::string rtrim(const std::string s)
 {
 	return std::regex_replace(s, std::regex("\\s+$"), "");
 }
 
 
-static std::string trim(const std::string s) {
+static inline std::string trim(const std::string s) {
 	return ltrim(rtrim(s));
 }
 
-static D3DXVECTOR4 lerp(D3DXVECTOR4 a, D3DXVECTOR4 b, float t) {
+static inline D3DXVECTOR4 lerp(D3DXVECTOR4 a, D3DXVECTOR4 b, float t) {
 	D3DXVECTOR4 result;
 	result.x = std::lerp(a.x, b.x, t);
 	result.y = std::lerp(a.y, b.y, t);
@@ -26,29 +26,29 @@ static D3DXVECTOR4 lerp(D3DXVECTOR4 a, D3DXVECTOR4 b, float t) {
 	return result;
 }
 
-static float clamp(float a, float b, float t) {
+inline static float clamp(float a, float b, float t) {
 	return min(max(a, t), b);
 }
 
-static float invLerp(float a, float b, float t) {
+inline static float invLerp(float a, float b, float t) {
 	return(t - a) / (b - a);
 }
 
-static float step(float a, float b, float t) {
+inline static float step(float a, float b, float t) {
 	return clamp(0, 1, invLerp(a, b, t));
 }
 
-static float smoothStep(float a, float b, float t) {
+inline static float smoothStep(float a, float b, float t) {
 	float t2 = step(a, b, t);
 	return t2 * t2 * (3.0 - 2.0 * t2);
 }
 
 // returns the sign of the int
-template <typename T> static int sgn(T val) {
+template <typename T> inline static int sgn(T val) {
 	return (T(0) < val) - (val < T(0));
 }
 
 // does a power operation which retains the sign
-static  float pows(float a, float b) {
+inline static  float pows(float a, float b) {
 	return (pow(abs(a), b) * sgn(a));
 }
