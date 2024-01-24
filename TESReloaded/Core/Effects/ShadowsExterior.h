98c2d1513078c76701e0bb888e02f7cc5ad8ee92
MathieuC <m.chaptel@gmail.com>
2024-01-25 00:15:23 +0100

    refactor RenderShadowMaps to centralise all settings for each cascade

--- TESReloaded/Core/Effects/ShadowsExterior.h
+++ TESReloaded/Core/Effects/ShadowsExterior.h
@@ -1,5 +1,18 @@
 #pragma once
 
+typedef struct frustum {
+	enum PLANE {
+		PLANENEAR = 0,
+		PLANEFAR = 1,
+		PLANELEFT = 2,
+		PLANERIGHT = 3,
+		PLANETOP = 4,
+		PLANEBOTTOM = 5,
+	};
+	PLANE PLANE;
+	D3DXPLANE plane[6];
+} frustum;
+
 class ShadowsExteriorEffect : public EffectRecord
 {
 public:
@@ -21,86 +34,95 @@ public:
 	
 		D3DXMATRIXA16	ShadowWorld;
 		D3DXMATRIX		ShadowViewProj;
-		D3DXMATRIX		ShadowCameraToLight[5];
 		D3DXVECTOR4		ShadowCubeMapLightPosition;
 		D3DXVECTOR4		ShadowLightPosition[ShadowCubeMapsMax];
 		D3DXVECTOR4		ShadowCubeMapBlend;
 		D3DXVECTOR4		ShadowMapRadius;
 	};
 
+	typedef std::vector<UInt32> ExcludedFormsList;
+
+	struct FormsStruct {
+		bool				Activators;
+		bool				Actors;
+		bool				Apparatus;
+		bool				Books;
+		bool				Containers;
+		bool				Doors;
+		bool				Furniture;
+		bool				Misc;
+		bool				Statics;
+		bool				Terrain;
+		bool				Trees;
+		bool				Lod;
+		float				MinRadius;
+	};
+
+	struct ShadowMapSettings {
+		D3DXMATRIX				ShadowCameraToLight;
+		IDirect3DTexture9*		ShadowMapTexture;
+		IDirect3DSurface9*		ShadowMapSurface;
+		IDirect3DSurface9*		ShadowMapDepthSurface;
+		D3DVIEWPORT9			ShadowMapViewPort;
+		frustum					ShadowMapFrustum;
+		IDirect3DVertexBuffer9* BlurShadowVertexBuffer;
+		FormsStruct				Forms;
+		float					ShadowMapInverseResolution;
+		float					ShadowMapRadius;
+		float					ShadowMapNear;
+		bool					AlphaEnabled;
+	};
+
+	struct ExteriorsStruct {
+		bool				Enabled;
+		bool                BlurShadowMaps;
+		int					Quality;
+		int					ShadowMapResolution;
+		float				ShadowRadius;
+		float				Darkness;
+		float				NightMinDarkness;
+		UInt8               ShadowMode;
+		float				ShadowMapFarPlane;
+		bool				UsePointShadowsDay;
+		bool				UsePointShadowsNight;
+		ExcludedFormsList	ExcludedForms;
+	};
+
+	struct InteriorsStruct {
+		bool				Enabled;
+		FormsStruct			Forms;
+		bool				AlphaEnabled;
+		bool				TorchesCastShadows;
+		int					LightPoints;
+		int					Quality;
+		int					ShadowCubeMapSize;
+		int					DrawDistance;
+		float				Darkness;
+		float				LightRadiusMult;
+		bool				UseCastShadowFlag;
+		ExcludedFormsList	ExcludedForms;
+		bool				PlayerShadowThirdPerson;
+		bool				PlayerShadowFirstPerson;
+	};
+
+	struct ScreenSpaceStruct {
+		bool				Enabled;
+		float				BlurRadius;
+		float				RenderDistance;
+	};
+
 	struct SettingsShadowStruct {
-		typedef std::vector<UInt32> ExcludedFormsList;
-
-		struct FormsStruct {
-			bool				Activators;
-			bool				Actors;
-			bool				Apparatus;
-			bool				Books;
-			bool				Containers;
-			bool				Doors;
-			bool				Furniture;
-			bool				Misc;
-			bool				Statics;
-			bool				Terrain;
-			bool				Trees;
-			bool				Lod;
-			float				MinRadius;
-		};
-
-		struct ExteriorsStruct {
-			bool				Enabled;
-			bool                BlurShadowMaps;
-			FormsStruct			Forms[5];
-			bool				AlphaEnabled[5];
-			int					Quality;
-			int					ShadowMapResolution;
-			float				ShadowRadius;
-			float				Darkness;
-			float				NightMinDarkness;
-			UInt8               ShadowMode;
-			float				ShadowMapRadius[5];
-			float				ShadowMapFarPlane;
-			bool				UsePointShadowsDay;
-			bool				UsePointShadowsNight;
-			ExcludedFormsList	ExcludedForms;
-		};
-
-		struct InteriorsStruct {
-			bool				Enabled;
-			FormsStruct			Forms;
-			bool				AlphaEnabled;
-			bool				TorchesCastShadows;
-			int					LightPoints;
-			int					Quality;
-			int					ShadowCubeMapSize;
-			int					DrawDistance;
-			float				Darkness;
-			float				LightRadiusMult;
-			bool				UseCastShadowFlag;
-			ExcludedFormsList	ExcludedForms;
-			bool				PlayerShadowThirdPerson;
-			bool				PlayerShadowFirstPerson;
-		};
-
-		struct ScreenSpaceStruct {
-			bool				Enabled;
-			float				BlurRadius;
-			float				RenderDistance;
-		};
-
 		ScreenSpaceStruct	ScreenSpace;
 		ExteriorsStruct		Exteriors;
 		InteriorsStruct		Interiors;
 	};
 	SettingsShadowStruct	Settings;
 	ShadowStruct			Constants;
+	ShadowMapSettings		ShadowMaps[5];
 
 	struct ShadowTextures {
 		IDirect3DTexture9* ShadowPassTexture;
 		IDirect3DSurface9* ShadowPassSurface;
-		IDirect3DTexture9* ShadowMapTexture[5];
-		IDirect3DSurface9* ShadowMapSurface[5];
-		IDirect3DSurface9* ShadowMapDepthSurface[5];
 		IDirect3DCubeTexture9* ShadowCubeMapTexture[ShadowCubeMapsMax];
 		IDirect3DSurface9* ShadowCubeMapSurface[ShadowCubeMapsMax][6];
 		IDirect3DSurface9* ShadowCubeMapDepthSurface;
@@ -113,5 +135,6 @@ public:
 	void		RegisterTextures();
 
 	void		GetCascadeDepths();
-	D3DXMATRIX	GetCascadeViewProj(ShadowMapTypeEnum ShadowMapType, SettingsShadowStruct::ExteriorsStruct* ShadowsExteriors, D3DXMATRIX View);
+	D3DXMATRIX	GetOrthoViewProj(D3DXMATRIX View);
+	D3DXMATRIX	GetCascadeViewProj(ShadowMapSettings* ShadowMap, D3DXMATRIX View);
 };
