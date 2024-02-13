cb99c246a3fea9cb81e793361997570af2f94e57
MathieuC <m.chaptel@gmail.com>
2024-02-13 23:20:47 +0100

    fix position/depth reconstruction when using a custom near clipping plane for the viewmodel

--- Extra/Shaders/NewVegasReloaded/Effects/CombineDepth.fx.hlsl
+++ Extra/Shaders/NewVegasReloaded/Effects/CombineDepth.fx.hlsl
@@ -1,3 +1,5 @@
+float4 TESR_DebugVar;
+
 sampler2D TESR_DepthBufferWorld : register(s0) = sampler_state { ADDRESSU = CLAMP; ADDRESSV = CLAMP; MAGFILTER = LINEAR; MINFILTER = LINEAR; MIPFILTER = LINEAR; };
 sampler2D TESR_DepthBufferViewModel : register(s1) = sampler_state { ADDRESSU = CLAMP; ADDRESSV = CLAMP; MAGFILTER = LINEAR; MINFILTER = LINEAR; MIPFILTER = LINEAR; };
 
@@ -21,11 +23,42 @@ VSOUT FrameVS(VSIN IN)
 	return OUT;
 }
 
+
+float4 TESR_CameraData;
+float4 TESR_CameraPosition;
+
+static const float nearZ = TESR_CameraData.x;
+static const float farZ = TESR_CameraData.y;
+
+// linearize depth
+float readDepth(float depth, float nearZ, float farZ)
+{
+	float Q = farZ/(farZ - nearZ);
+    float ViewZ = (-nearZ *Q) / (depth - Q);
+	return ViewZ;
+}
+
+// convert back to usual depth buffer format for easier reconstruction
+float packDepth(float viewZ, float nearZ, float farZ){
+	float Q = farZ/(farZ - nearZ);
+	return (Q* (viewZ - nearZ ))/ viewZ;
+}
+
+
 float4 CombineDepth(VSOUT IN) : COLOR0
 {	
 	float worldDepth = tex2D(TESR_DepthBufferWorld, IN.UVCoord).x;
-	float viewModelDepth = tex2D(TESR_DepthBufferViewModel, IN.UVCoord).y;
-	return float4(min(worldDepth, viewModelDepth), worldDepth, viewModelDepth, 1.0);
+	float viewModelDepth = tex2D(TESR_DepthBufferViewModel, IN.UVCoord).x;
+	
+	float linearWorldDepth = readDepth(worldDepth, nearZ, farZ);
+	float linearViewModelDepth = readDepth(viewModelDepth, TESR_DebugVar.x, farZ);
+
+	// return float4(min(linearWorldDepth, linearViewModelDepth), min(worldDepth, viewModelDepth), 1.0, 1.0) ;
+	float depth = min(linearWorldDepth, linearViewModelDepth);
+	float packedDepth = packDepth(depth, nearZ, farZ);
+	
+	return float4(depth / farZ, packedDepth, 1.0, 1.0) ;
+	// return float4(depth, packDepth(depth, nearZ, farZ), 1.0, 1.0) ;
 }
  
 technique
