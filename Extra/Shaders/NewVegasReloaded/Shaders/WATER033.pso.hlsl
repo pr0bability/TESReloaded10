2bd51ae82bfd42e96146f4fb97f6905becabf343
MathieuC <m.chaptel@gmail.com>
2024-02-14 14:28:49 +0100

    revert test with water LOD coloration

--- Extra/Shaders/NewVegasReloaded/Shaders/WATER033.pso.hlsl
+++ Extra/Shaders/NewVegasReloaded/Shaders/WATER033.pso.hlsl
@@ -53,8 +53,8 @@ PS_OUTPUT main(PS_INPUT IN) {
     PS_OUTPUT OUT;
 
     float4 linSunColor = linearize(SunColor); //linearise
-    //float4 linShallowColor = linearize(ShallowColor); //linearise
-    float4 linShallowColor = linearize(TESR_WaterShallowColor); //linearise
+    float4 linShallowColor = linearize(ShallowColor); //linearise
+    //float4 linShallowColor = linearize(TESR_WaterShallowColor); //linearise
     float4 linHorizonColor = linearize(TESR_HorizonColor); //linearise
 
     float3 eyeVector = EyePos.xyz - IN.LTEXCOORD_0.xyz; // vector of camera position to point being shaded
