d5a8332cda5ab8369ce4facdab04dc65163f51f9
MathieuC <m.chaptel@gmail.com>
2024-02-18 01:57:41 +0100

    new structure, more standard

--- build.bat
+++ build.bat
@@ -37,17 +37,17 @@ xcopy "%PROJECT%\Release\%PROJECT%.dll" "build\%PROJECT%\%FOLDER%\Plugins\" /y /
 xcopy "%PROJECT%\Release\%PROJECT%.pdb" "build\%PROJECT%\%FOLDER%\Plugins\" /y /d
 
 @REM Copy config
-xcopy "resources\%PROJECT%.dll.config" "build\%PROJECT%\%FOLDER%\Plugins\" /y /d
-xcopy "resources\%PROJECT%.dll.defaults.toml" "build\%PROJECT%\%FOLDER%\Plugins\" /y /d
-echo F|xcopy "resources\%PROJECT%.dll.defaults.toml" "build\%PROJECT%\%FOLDER%\Plugins\%PROJECT%.dll.toml" /y /d
+xcopy "resource\%PROJECT%.dll.config" "build\%PROJECT%\%FOLDER%\Plugins\" /y /d
+xcopy "resource\%PROJECT%.dll.defaults.toml" "build\%PROJECT%\%FOLDER%\Plugins\" /y /d
+echo F|xcopy "resource\%PROJECT%.dll.defaults.toml" "build\%PROJECT%\%FOLDER%\Plugins\%PROJECT%.dll.toml" /y /d
 
 @REM Copy Shaders
 set GAME=%PROJECT:Reloaded=""%
-robocopy /mir .\%GAME%\Shaders .\build\%PROJECT%\Shaders\%PROJECT%\Shaders
-robocopy /mir .\%GAME%\Effects .\build\%PROJECT%\Shaders\%PROJECT%\Effects
+robocopy /mir .\src\hlsl\%GAME%\Shaders .\build\%PROJECT%\Shaders\%PROJECT%\Shaders
+robocopy /mir .\src\hlsl\%GAME%\Effects .\build\%PROJECT%\Shaders\%PROJECT%\Effects
 
 @REM Copy Textures
-robocopy /mir .\resources\Textures\ .\build\%PROJECT%\Textures\
+robocopy /mir .\resource\Textures\ .\build\%PROJECT%\Textures\
 
 @REM Copy to deploy if 2nd arg was passed
 if "%DEPLOY_LOCATION%" NEQ "" (
