c04b22e78826f1d8a7d53dc341f1e7af3c2accc6
MathieuC <m.chaptel@gmail.com>
2024-02-19 22:43:15 +0100

    render bloom after exposure, move rain and snow back to post tonemapping

--- src/effects/Bloom.cpp
+++ src/effects/Bloom.cpp
@@ -1,10 +1,18 @@
 #include "Bloom.h"
 
-void BloomEffect::RegisterConstants() {};
+void BloomEffect::RegisterConstants() {
+	TheShaderManager->RegisterConstant("TESR_BloomData", &Constants.Data);
+};
 
 void BloomEffect::RegisterTextures() {
 	TheTextureManager->InitTexture("TESR_BloomBuffer", &Textures.BloomTexture, &Textures.BloomSurface, TheRenderManager->width / 2, TheRenderManager->height /2, D3DFMT_A16B16G16R16F);
 };
 
-void BloomEffect::UpdateSettings() {};
+void BloomEffect::UpdateSettings() {
+	Constants.Data.x = TheSettingManager->GetSettingF("Shaders.Bloom.Main", "Treshold");
+	Constants.Data.y = TheSettingManager->GetSettingF("Shaders.Bloom.Main", "Scale");
+	Constants.Data.z = TheSettingManager->GetSettingF("Shaders.Bloom.Main", "Strength");
+	Constants.Data.w = Enabled;
+};
+
 void BloomEffect::UpdateConstants() {};
