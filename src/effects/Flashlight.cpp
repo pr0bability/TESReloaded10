345b3db12771dcb4e8483ede0e79ef5bfa20c7d8
MathieuC <m.chaptel@gmail.com>
2024-02-26 21:33:10 +0100

    store a spotlight reference in Flashlight effect

--- src/effects/Flashlight.cpp
+++ src/effects/Flashlight.cpp
@@ -2,8 +2,28 @@
 
 void FlashlightEffect::RegisterConstants() {};
 
-void FlashlightEffect::UpdateSettings() {};
+void FlashlightEffect::UpdateSettings() {
+	Logger::Log("Creating spotlight");
+	return;
+	if (!SpotLight)
+		SpotLight = NiSpotLight::CreateObject();
+};
 
-void FlashlightEffect::UpdateConstants() {};
+void FlashlightEffect::UpdateConstants() {
+
+	if (SpotLight) {
+		Logger::Log("Setting up spotlight");
+
+		SpotLight->Diff = NiColor(1, 1, 0.5);
+		SpotLight->Dimmer = 1.0f;
+		SpotLight->m_worldTransform.pos = NiPoint3(TheRenderManager->CameraPosition.x, TheRenderManager->CameraPosition.y, TheRenderManager->CameraPosition.z + 100);
+		SpotLight->m_worldTransform.rot = WorldSceneGraph->camera->m_worldTransform.rot;
+		SpotLight->m_worldTransform.scale = 1.0f;
+		SpotLight->OuterSpotAngle = 20.0f;
+		SpotLight->Spec = NiColor(1000, 0, 0); // radius in r channel
+	}
+
+};
 
 bool FlashlightEffect::ShouldRender() { return true; };
+
