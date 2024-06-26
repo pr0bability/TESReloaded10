ca2cbf1cbfc97c01a9cd57ba5c119880d53271fc
MathieuC <m.chaptel@gmail.com>
2024-06-26 20:55:03 +0200

    add rain and night settings for terrain

--- src/effects/Terrain.h
+++ src/effects/Terrain.h
@@ -5,6 +5,23 @@ class TerrainShaders : public ShaderCollection
 public:
 	TerrainShaders() : ShaderCollection("Terrain") {};
 
+	struct TerrainSettings {
+		float LightScale;
+		float AmbientScale;
+		float Roughness;
+		float Metallicness;
+		float Specular;
+		float Fresnel;
+		float Saturation;
+	};
+	struct TerrainSettingsStruct {
+		TerrainSettings Default;
+		TerrainSettings Rain;
+		TerrainSettings Night;
+		TerrainSettings NightRain;
+	};
+	TerrainSettingsStruct Settings;
+
 	struct TerrainStruct {
 		D3DXVECTOR4		Data;
 		D3DXVECTOR4		ExtraData;
@@ -12,6 +29,7 @@ public:
 	TerrainStruct	Constants;
 
 	bool			usePBR;
+	float			LodNoiseScale;
 
 	void	UpdateConstants();
 	void	RegisterConstants();
