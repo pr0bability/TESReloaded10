00352625dc84bfef21310359fb8c9c85abc4135f
MathieuC <m.chaptel@gmail.com>
2024-08-03 18:45:35 +0700

    fix issue in PBR formula

--- src/hlsl/NewVegas/Effects/Includes/PBR.hlsl
+++ src/hlsl/NewVegas/Effects/Includes/PBR.hlsl
@@ -18,35 +18,35 @@ float GeometryShadowing(float alpha, float NdotV, float NdotL){
 }
 
 // F
-float3 FresnelShlick(float3 reflectance, float3 halfway, float3 lightDir){
-    return reflectance + (1 - reflectance) * pow(1 - shades(halfway, lightDir), 5.0);
+float3 FresnelShlick(float3 reflectance, float3 halfway, float3 eyeDir){
+    return reflectance + (1 - reflectance) * pow(1 - shades(halfway, eyeDir), 5.0);
 }
 
 float3 CookTorrance(float alpha, float3 fresnel, float NdotV, float NdotL, float NdotH){
     float3 num = GGX(alpha, NdotH) * GeometryShadowing(alpha, NdotV, NdotL) * fresnel;
-    float denom = max(4 * NdotV * NdotL, 0.0001);
+    float denom = max(4 * NdotV * NdotL, 0.0000001);
     return num/denom;
 }
 
 
 float3 BRDF(float roughness, float NdotL, float NdotV, float NdotH, float3 fresnel){
-    return fresnel + (1 - fresnel) * CookTorrance(roughness, fresnel, NdotV, NdotL, NdotH);
+    return fresnel * CookTorrance(roughness, fresnel, NdotV, NdotL, NdotH);
 }
 
 float3 modifiedBRDF(float roughness, float NdotL, float NdotV, float NdotH, float3 fresnel){
     // BRDF without the fresnel term to get cleaner specular on water
-    float3 BRDF = (1 - fresnel) * CookTorrance(roughness, fresnel, NdotV, NdotL, NdotH);
+    float3 BRDF = fresnel * CookTorrance(roughness, fresnel, NdotV, NdotL, NdotH);
     return BRDF * NdotL;
 }
 
 float3 PBR(float metallicness, float roughness, float3 albedo, float3 normal, float3 eyeDir, float3 lightDir, float3 lightColor){
     float3 reflectance = lerp(float(0.04).rrr, albedo, metallicness);
     float3 halfway = normalize(eyeDir + lightDir);
-    float NdotL = shades(normal, lightDir);
-    float NdotV = shades(normal, eyeDir);
+    float NdotL = max(shade(normal, lightDir), 0.0000001);
+    float NdotV = max(shade(normal, eyeDir), 0.0000001);
     float NdotH = shades(normal, halfway);
 
-    float3 Ks = FresnelShlick(reflectance, halfway, lightDir);
+    float3 Ks = FresnelShlick(reflectance, halfway, eyeDir);
     float3 lambertDiffuse = (1 - metallicness) * (1 - Ks) * albedo/PI;
     float3 spec = BRDF(roughness * roughness, NdotL, NdotV, NdotH, Ks);
     return (lambertDiffuse + spec) * NdotL * lightColor;
