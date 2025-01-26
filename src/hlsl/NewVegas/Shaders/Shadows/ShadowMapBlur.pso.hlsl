15b7f018dda5941f5b41417989b224af6b58477e
pr0bability <pr0babilityiirc@gmail.com>
2025-01-26 16:38:28 +0100

    Clamp shadow blur coordinates per cascade
    Avoid blurring accross cascade borders in the atlas.

--- src/hlsl/NewVegas/Shaders/Shadows/ShadowMapBlur.pso.hlsl
+++ src/hlsl/NewVegas/Shaders/Shadows/ShadowMapBlur.pso.hlsl
@@ -8,48 +8,57 @@ struct VSOUT
 	float2 UVCoord : TEXCOORD0;
 };
 
-float4 Blur3(float2 uv) {
+float4 Blur3(float2 uv, float2 upperLeft, float2 lowerRight) {
     float weights[1] = { 0.5f };
     float offsets[1] = { 0.6f };
     
     float2 offset = BlurDirection * offsets[0] * TESR_ReciprocalResolution.x;
 
-    float4 color = tex2Dlod(SourceBuffer, float4(uv.xy + offset, 0.0f, 0.0f)) * weights[0];
-    color += tex2Dlod(SourceBuffer, float4(uv.xy - offset, 0.0f, 0.0f)) * weights[0];
+    float2 offsetUV = clamp(uv.xy + offset, upperLeft, lowerRight);
+    float4 color = tex2Dlod(SourceBuffer, float4(offsetUV, 0.0f, 0.0f)) * weights[0];
+    
+    offsetUV = clamp(uv.xy - offset, upperLeft, lowerRight);
+    color += tex2Dlod(SourceBuffer, float4(offsetUV, 0.0f, 0.0f)) * weights[0];
 
     return color;
 }
 
-float4 Blur5(float2 uv) {
+float4 Blur5(float2 uv, float2 upperLeft, float2 lowerRight) {
     float weights[2] = { 0.2941176470f, 0.3529411764f };
     float offsets[2] = { 0.0f, 1.3333333333f };
     
     float4 color = tex2Dlod(SourceBuffer, float4(uv.xy, 0.0f, 0.0f)) * weights[0];
     
-    float2 offset;
+    float2 offset, offsetUV;
     for (int i = 1; i < 2; i++) {
         offset = BlurDirection * offsets[i] * TESR_ReciprocalResolution.x;
         
-        color += tex2Dlod(SourceBuffer, float4(uv.xy + offset, 0.0f, 0.0f)) * weights[i];
-        color += tex2Dlod(SourceBuffer, float4(uv.xy - offset, 0.0f, 0.0f)) * weights[i];
+        offsetUV = clamp(uv.xy + offset, upperLeft, lowerRight);
+        color += tex2Dlod(SourceBuffer, float4(offsetUV, 0.0f, 0.0f)) * weights[i];
+        
+        offsetUV = clamp(uv.xy - offset, upperLeft, lowerRight);
+        color += tex2Dlod(SourceBuffer, float4(offsetUV, 0.0f, 0.0f)) * weights[i];
     }
     
     return color;
 }
 
 
-float4 Blur9(float2 uv) {
+float4 Blur9(float2 uv, float2 upperLeft, float2 lowerRight) {
     float weights[3] = { 0.2270270270f, 0.3162162162f, 0.0702702703f };
     float offsets[3] = { 0.0f, 1.3846153846f, 3.2307692308f };
     
     float4 color = tex2Dlod(SourceBuffer, float4(uv.xy, 0.0f, 0.0f)) * weights[0];
     
-    float2 offset;
+    float2 offset, offsetUV;
     for (int i = 1; i < 3; i++) {
         offset = BlurDirection * offsets[i] * TESR_ReciprocalResolution.x;
         
-        color += tex2Dlod(SourceBuffer, float4(uv.xy + offset, 0.0f, 0.0f)) * weights[i];
-        color += tex2Dlod(SourceBuffer, float4(uv.xy - offset, 0.0f, 0.0f)) * weights[i];
+        offsetUV = clamp(uv.xy + offset, upperLeft, lowerRight);
+        color += tex2Dlod(SourceBuffer, float4(offsetUV, 0.0f, 0.0f)) * weights[i];
+        
+        offsetUV = clamp(uv.xy - offset, upperLeft, lowerRight);
+        color += tex2Dlod(SourceBuffer, float4(offsetUV, 0.0f, 0.0f)) * weights[i];
     }
     
     return color;
@@ -69,11 +78,11 @@ float4 main(VSOUT IN) : COLOR0 {
     float2 uv = IN.UVCoord.xy;
     
     [branch] if (uv.x < 0.5 && uv.y < 0.5)
-        return Blur9(uv);  // Closest cascade.
+        return Blur9(uv, float2(0.0f, 0.0f), float2(0.5f, 0.5f)); // Closest cascade.
     else if (uv.y < 0.5)
-        return Blur5(uv);  // Second cascade.
+        return Blur5(uv, float2(0.5f, 0.0f), float2(1.0f, 0.5f)); // Second cascade.
     else if (uv.x < 0.5)
-        return Blur3(uv);  // Third cascade.
+        return Blur3(uv, float2(0.0f, 0.5f), float2(0.5f, 1.0f)); // Third cascade.
     else
-        return Blur3(uv);  // Furthest cascade.
+        return Blur3(uv, float2(0.5f, 0.5f), float2(1.0f, 1.0f)); // Furthest cascade.
 }
