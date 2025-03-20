45ecd469b50adb2f31e74e2549f77d50b7b2fe50
pr0bability <pr0babilityiirc@gmail.com>
2025-03-20 23:57:49 +0100

    Get rid of LoadGame hook in favor of NVSE message

--- src/NewVegas/Hooks/Settings.cpp
+++ src/NewVegas/Hooks/Settings.cpp
@@ -46,19 +46,6 @@ bool __fastcall WriteSettingHook(INISettingCollection* This, UInt32 edx, GameSet
 	
 }
 
-bool (__thiscall* LoadGame)(TESSaveLoadGame*, char*, UInt8) = (bool (__thiscall*)(TESSaveLoadGame*, char*, UInt8))Hooks::LoadGame;
-bool __fastcall LoadGameHook(TESSaveLoadGame* This, UInt32 edx, char* FileName, UInt8 Arg2) {
-
-	bool r;
-	
-	TheSettingManager->GameLoading = true;
-	r = (*LoadGame)(This, FileName, Arg2);
-	TheSettingManager->GameLoading = false;
-	if (r) TheShaderManager->InitializeConstants();
-	return r;
-
-}
-
 void MainMenuMusicFix(char* DestinationBuffer, size_t Size, char* SourceBuffer) {
 
 	char Buffer[64] = { NULL };
